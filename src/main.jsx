121f588312a80c3d55f04b3b6e5e81837a4db158
Wubet Alebachew <wubet453@gmail.com>
2025-11-21 22:35:56 +0300

    Update routing for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter } from 'react-router-dom'
 import StoreContextProvider from './Context/StoreContext.jsx'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <StoreContextProvider>
       <App />
   </StoreContextProvider>
   
-    </BrowserRouter>
+    </HashRouter>
   
 )
